38b2eaca50c5cf38546475687e8bdc8dd5198653
moniasuka <moniasuka@hotmail.com>
2022-09-17 03:13:22 +0000

    pushing domain generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let con1 = ["lava", "world", "fancy", "twist"];
+  let con2 = ["con", "gate", "fit", "push"];
+  let con3 = [".com", ".net", ".edu", ".org"];
+
+  let textoh1 = document.querySelector("#domain");
+
+  let placeholder = "";
+
+  for (let i = 0; i < con1.length; i++) {
+    for (let j = 0; j < con3.length; j++) {
+      console.log(con1[i] + " " + con3[j]);
+      placeholder +=
+        "<p>" +
+        con1[i] +
+        con2[i] +
+        con3[j] +
+        "</p>" +
+        "<p>" +
+        con2[i] +
+        con1[i] +
+        con3[j] +
+        "</p>";
+    }
+  }
+  textoh1.innerHTML = placeholder;
 };
